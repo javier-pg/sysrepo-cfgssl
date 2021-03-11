4af9f8c7242a3706c0e1d56b81ecfdb009d10059
javier-pg <javierpg@um.es>
2021-03-11 14:31:11 +0100

    Update README

--- mesh_experiments.sh
+++ mesh_experiments.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # experiment parameters
+initial_mesh_size=5
+mesh_step=5
 max_mesh_size=80
 number_experiments_per_mesh=20
 
 #echo "Running $number_experiments experiments for each mesh network, from 2 node to $max_mesh_size nodes"
 
 # run the specified number of experiments for each mesh size
-for mesh in `seq 5 5 $max_mesh_size`;
+for mesh in `seq $initial_mesh_size $mesh_step $max_mesh_size`;
 do
 	echo "Experiments for mesh with $mesh nodes..."
 	./mesh.sh $mesh $number_experiments_per_mesh
